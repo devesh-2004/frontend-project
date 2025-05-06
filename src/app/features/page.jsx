f4d862cba622b59d47155517c28160c601325aa9
devesh-2004 <deveshpurohit275@gmail.com>
2025-05-06 06:30:27 +0530

    Created features

---
+++ src/app/features/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-screen h-screen bg-green-500 text-8xl text-center flex items-center justify-center'>Features</div>
+  )
+}
+
+export default page
